b18ed5380bd8429788f316df493369e3d8706a77
Frostlag <dingtaoyin@hotmail.com>
2015-01-24 10:45:47 -0500

    Hopefully Final push before branch

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Conductor.instance.SendMessage ("Register", gameObject);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		// See if we can handle input
+		Conductor.instance.SendMessage ("CanInput", gameObject,"handleControls");
+
+	}
+
+	void handleControls(){
+		if (Input.GetKeyDown("space")){
+			print ("Space");
+		}
+	}
+
+	void Beat(){
+		
+	}
+}
